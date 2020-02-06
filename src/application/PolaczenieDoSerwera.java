d6075729ef0898ef070b8285f59b491ad4ac0690
bartoszsol <ulvhedin@go2.pl>
2020-02-06 21:57:33 +0100

    glowna gra nie ruszona, zmieniony serwer i klient - jako oddzielne aplikacje do przesylania obiektu przez socket - dziala

--- src/application/PolaczenieDoSerwera.java
+++ src/application/PolaczenieDoSerwera.java
@@ -2,26 +2,36 @@ package application;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PolaczenieDoSerwera {
 	
 	
-	public void wyslijWynik(Gracz gracz) throws IOException {
+	//public void wyslijWynik(Gracz gracz) throws IOException {
+	public static void main(String[] args) throws IOException {
 		
-		// Server will be started on 1700 port number
-	    ServerSocket server=new ServerSocket(1700);
-		
-	   // Server listening for connection
-	    Socket s=server.accept();
-		
-	   // Stream object for sending object 
-	    ObjectOutputStream os=new ObjectOutputStream(s.getOutputStream());
-		
-	   os.writeObject(gracz);
-	   
-	   s.close();
-	   server.close();
+		// need host and port, we want to connect to the ServerSocket at port 7777
+        Socket socket = new Socket("localhost", 7777);
+        System.out.println("Connected!");
+
+        // get the output stream from the socket.
+        OutputStream outputStream = socket.getOutputStream();
+        // create an object output stream from the output stream so we can send an object through it
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+        // make a bunch of messages to send.
+        List<Gracz> gracze = new ArrayList<>();
+        gracze.add(new Gracz("gracz1", 101));
+        gracze.add(new Gracz("gracz2", 102));
+        gracze.add(new Gracz("gracz3", 103));
+
+        System.out.println("Sending messages to the ServerSocket");
+        objectOutputStream.writeObject(gracze);
+
+        System.out.println("Closing socket and terminating program.");
+        socket.close();
 	}
 }
--- src/application/SerwerWynikow.java
+++ src/application/SerwerWynikow.java
@@ -1,22 +1,41 @@
 package application;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class SerwerWynikow {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-
-	     Socket s=new Socket("127.0.0.1",1700);
-	     ObjectInputStream is=new ObjectInputStream(s.getInputStream());
-	     Gracz gracz=(Gracz)is.readObject();
-	     
-	     System.out.println("-----------------------serwer printuje :" + gracz.toString());
-	     
-	     s.close();
 		
+        // don't need to specify a hostname, it will be the current machine
+        ServerSocket ss = new ServerSocket(7777);
+        System.out.println("ServerSocket awaiting connections...");
+        Socket socket = ss.accept(); // blocking call, this will wait until a connection is attempted on this port.
+        System.out.println("Connection from " + socket + "!");
+
+        // get the input stream from the connected socket
+        InputStream inputStream = socket.getInputStream();
+        // create a DataInputStream so we can read data from it.
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+        // read the list of messages from the socket
+		List<Gracz> listaGraczy = (List<Gracz>) objectInputStream.readObject();
+        System.out.println("Received [" + listaGraczy.size() + "] messages from: " + socket);
+        // print out the text of every message
+        System.out.println("All messages:");
+        
+		for (Gracz gracz : listaGraczy) {
+			System.out.println("-----------------------serwer printuje :" + gracz.toString());
+		}
+        
+
+        System.out.println("Closing sockets.");
+        ss.close();
+        socket.close();
 
 	}
 }
